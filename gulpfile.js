4581058eb34b407165099551fb26a5e709ec3786
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2021-06-04 16:59:21 +0800

    update service worker

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require("gulp");
+const shell = require("gulp-shell");
+const workbox = require("workbox-build");
+
+gulp.task("hugo-build", shell.task(["hugo --gc --minify --cleanDestinationDir --verbose"])); // this can change depending on which static generator you are using.
+
+gulp.task("generate-service-worker", () => {
+    return workbox.generateSW({
+        cleanupOutdatedCaches: true,
+        mode: "production",
+        sourcemap: true,
+        globDirectory: "./public", // the directory where static file is stored
+        swDest: "./public/sw.js", // destination of sw.js file
+        globPatterns: ["**/*.{js,css,html}"],
+        navigateFallback: "/404.html", // fallback file
+        globIgnores: ["**/index.xml", "**/index.json", "**/sitemap.xml"],
+        runtimeCaching: [
+            {
+                urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+                handler: "CacheFirst",
+            },
+            {
+                urlPattern: /\.(?:json|xml)$/,
+                handler: "NetworkOnly",
+            },
+        ],
+    });
+});
+
+gulp.task("build", gulp.series("hugo-build", "generate-service-worker"));
