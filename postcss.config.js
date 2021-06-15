577d10e76e6af8910ce714f2046789283ceb491f
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2021-06-15 19:48:20 +0800

    add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    plugins: [
-        require('cssnano')({
-            preset: 'advanced',
-        }),
-    ],
-};
+    map: false,
+    plugins: {
+      '@fullhuman/postcss-purgecss': {
+        content: ['./themes/**/*.html', './themes/**/*.js', './themes/liva-hugo/assets/plugins/themify-icons/themify-icons.css'],
+      },
+    },
+    cssnano: {preset: 'advanced'}
+  };
