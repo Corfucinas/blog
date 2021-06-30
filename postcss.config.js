51120c11fda61c73f1076cff08a745cb4dac59e7
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2021-07-01 02:34:44 +0800

    add structured SEO and breadcrumbs

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: {
     '@fullhuman/postcss-purgecss': {
       content: ['./themes/**/*.html', './themes/liva-hugo/assets/plugins/bootstrap/**/*.js', './themes/liva-hugo/assets/js/**/*.js', './themes/liva-hugo/assets/plugins/darkmode/darkmode.1.5.7.min.js'],
+      css: ['./themes/**/*.css'],
       safelist: ['blockquote', 'ti-email', 'ti-github', 'ti-linkedin'],
       fontFace: true,
       keyframes: true,
